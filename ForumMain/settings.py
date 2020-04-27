a71e966e185159e4bd881ee4494673340a5a7a31
james.zhan@3ti.us <james.zhan@3ti.us>
2020-04-27 22:13:17 +0800

    。。

--- ForumMain/settings.py
+++ ForumMain/settings.py
@@ -6,4 +6,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 settings = {
     "MEDIA_ROOT": os.path.join(BASE_DIR, "media"),
+    "static_path": os.path.join(BASE_DIR,"static"),
+    "template_path": os.path.join(BASE_DIR,"templates"),
 }
